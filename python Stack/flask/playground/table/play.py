ff32257ce334419c7419e5788a1da5c0d95d4b0d
Barooq23 <barqlaith99@gmail.com>
2022-09-18 15:05:02 +0300

    new py

--- python Stack/flask/playground/table/play.py
+++ python Stack/flask/playground/table/play.py
@@ -9,20 +9,20 @@ def initial_render():
 
 @app.route("/play")
 def block_render():
-    return render_template('play1.html')
+    return render_template('play1.html', repeat=3, colorChange="skyblue")
 
 
 @app.route("/play/<number_of_boxes>")
 def block_repeat(number_of_boxes):
     repeat = int(number_of_boxes)
-    return render_template('play2.html', repeat=repeat)
+    return render_template('play1.html', repeat=repeat, colorChange="skyblue")
 
 
 @app.route("/play/<number_of_boxes>/<color_change>")
 def box_color(number_of_boxes, color_change):
     repeat = (int(number_of_boxes))
     colorChange = color_change
-    return render_template('play3.html', repeat=repeat, colorChange=colorChange)
+    return render_template('play1.html', repeat=repeat, colorChange=colorChange)
 
 
 if __name__ == "__main__":
