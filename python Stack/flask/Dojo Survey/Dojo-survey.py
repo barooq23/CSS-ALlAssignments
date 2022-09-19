98fb6f26b15b31342440ee929de8ebb30eaccf00
Barooq23 <barqlaith99@gmail.com>
2022-09-19 15:04:38 +0300

    edit

---
+++ python Stack/flask/Dojo Survey/Dojo-survey.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("Dojo-survey.html")
+
+
+@app.route('/users', methods=['POST', 'GET'])
+def create_user():
+    if request.method == 'POST':
+        name_from_form = request.form['name']
+        email_from_form = request.form['email']
+        location = request.form['location']
+        language = request.form['fav_lang']
+        comment = request.form['comment']
+        return render_template("show.html", name_on_template=name_from_form, email_on_template=email_from_form, locat = location, lang = language, com = comment)
+    else:
+        return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
