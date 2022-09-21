164661431e9cddb35d2f46391c30b5cf803e6aef
Barooq23 <barqlaith99@gmail.com>
2022-09-21 13:04:04 +0300

    new flask edit

---
+++ python Stack/flask/counter/counter.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'lkjhfdfsdfs'
+
+
+@app.route('/')
+def index():
+
+    if 'count' not in session:
+        session['count'] = 0
+    else:
+        session['count'] += 1
+        
+    return render_template("counter.html")
+
+
+@app.route('/addtwo')
+def addtwo():
+    session['count'] += 1
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session['count'] = 0
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
