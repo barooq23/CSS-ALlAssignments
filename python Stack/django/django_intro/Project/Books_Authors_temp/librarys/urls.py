3c21a1f17955f9ece3cac4a26445b23d7b0f22af
Barooq23 <barqlaith99@gmail.com>
2022-09-30 19:06:56 +0300

    auther_book

--- python Stack/django/django_intro/Project/Books_Authors_temp/librarys/urls.py
+++ python Stack/django/django_intro/Project/Books_Authors_temp/librarys/urls.py
@@ -4,5 +4,9 @@ urlpatterns = [
     path('', views.book),
     path('addbook', views.addbook),
     path('books/<int:id>', views.viewbook),
-    path('authtobook/<int:id>', views.authtobook)
+    path('authtobook/<int:id>', views.authtobook),
+    path('auth', views.auther),
+    path('addauther', views.addauther),
+    path('authers/<int:id>', views.viewaouthe),
+    path('bookauth/<int:id>',views.addauth),
 ]
--- python Stack/django/django_intro/Project/Books_Authors_temp/librarys/views.py
+++ python Stack/django/django_intro/Project/Books_Authors_temp/librarys/views.py
@@ -1,26 +1,59 @@
 from multiprocessing import context
-from turtle import title
 from django.shortcuts import render, redirect
 from .models import *
 # Create your views here.
+
+
 def book(request):
-    context={
-        'books':Book.objects.all()
+    context = {
+        'books': Book.objects.all()
     }
-    return render (request, 'book.html', context)
+    return render(request, 'book.html', context)
+
+
 def addbook(requset):
     Book.objects.create(title=requset.POST['title'])
     return redirect('/')
+
+
+def authtobook(request, id):
+    book1 = Book.objects.get(id=int(id))
+    pub1 = Author.objects.get(id=request.POST['authtobook'])
+    book1.authors.add(pub1)
+    return redirect(f'/books/{book1.id}')
+
+
+def auther(request):
+    context = {
+        'authers': Author.objects.all()
+    }
+    return render(request, 'author.html', context)
+
+
+def addauther(request):
+    Author.objects.create(
+        first_name=request.POST['first_name'], last_name=request.POST['last_name'])
+    return redirect('/auth')
+
+
+def viewaouthe(request, id):
+    context = {
+        'auth': Author.objects.get(id=int(id)),
+        'allbook': Book.objects.all()
+    }
+    return render(request, 'authinfo.html', context)
+
+
 def viewbook(request, id):
-    context={
+    context = {
         'book': Book.objects.get(id=int(id)),
         'allauthor': Author.objects.all()
     }
     return render(request, 'information.html', context)
 
 
-def authtobook(request, id):
-    book1= Book.objects.get(id=int(id))
-    pub1 = Author.objects.get(id=request.POST['authtobook'])
-    book1.author.add(pub1)
-    return redirect ('/books/'+str(id))
+def addauth(request,id):
+    auth1 = Author.objects.get(id=int(id))
+    book1 = Book.objects.get(id=request.POST['authtobook'])
+    auth1.books.add(book1)
+    return redirect(f'/authers/{auth1.id}')
