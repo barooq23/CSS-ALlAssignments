9b5b05ec0c16dbbb3560ff2d2cce58783e2cfc4f
Barooq23 <barqlaith99@gmail.com>
2022-10-06 08:16:07 +0300

    favebook

---
+++ python Stack/django/django_intro/Project/fave_book/faves/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.root),
+    path('reg', views.reg),
+    path('books', views.books),
+    path('login', views.login),
+    path('addbook', views.addbook),
+    path('logout', views.logout),
+    path('favbooks/<int:id>', views.favbooks),
+    path('addfav/<int:id>', views.addfav),
+    path('unfav/<int:id>', views.unfav),
+    path('update/<int:id>', views.update),
+    path('addfav2/<int:id>', views.addfav2),
+]
